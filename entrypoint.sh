7d62059aa1438363694c82f8ded97cf165fa8b0d
dmasood-aslase <141400255+dmasood-aslase@users.noreply.github.com>
2024-02-06 19:26:30 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #Sync files with amazon s3 bucket app
-aws s3 sync ./dist s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --region $INPUT_AWS_DEFAULT_REGION --delete
+aws s3 sync ./dist s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --delete
